764b7a8526e04d22c9b41a1f9d865fb19b4e4373
ditam <ditam@users.noreply.github.com>
2020-04-20 14:56:13 +0200

    improve upgrade panel layout and styling

--- scene_upgrades.js
+++ scene_upgrades.js
@@ -22,66 +22,70 @@ game.upgrades = {
 			},
 			{
 				id: 'up_b2',
-				name: 'Chlorophyll Reabsorbption',
+				name: 'Chlorophyll Reabsorption',
 				icon: 'icon-leaves',
-				description: 'Use guard cells to open and close the stomata during the day, improving energy efficiency.',
+				description: 'Before dropping leaves, reabsorb their chrolophyll cells to regain resources.',
 				costs: [100, 100, 50],
 				bought: false,
 			}
 		]
 	},
 	'trunk': {
-		description: 'This is important too...',
+		description: 'The trunk provides support and height to the branches. The undifferentiated ' +
+									'<span class="resource stem">meristematic cells</span> are the basic building blocks of ' +
+									'all other organs and tissues of the plant.',
 		items: [
 			{
 				id: 'up_t0',
-				name: 'Trunk Name 0',
-				icon: 'icon-branch',
-				description: 'Trunk Desc 0',
+				name: 'Lignified Cell Walls',
+				icon: 'icon-cells',
+				description: 'A fibrous, strengthened vascular body provides structure and improved nutrient storage.',
 				costs: [50, 100, 200],
 				bought: false,
 			},
 			{
 				id: 'up_t1',
-				name: 'Trunk Name 1',
-				icon: 'icon-light',
-				description: 'Trunk Desc 1',
+				name: 'Protective Terpenes',
+				icon: 'icon-biohazard',
+				description: 'Terpenes are toxic to pathogens, protecting the heartwood from bacteria and fungi.',
 				costs: [50, 100, 200],
 				bought: false,
 			},
 			{
 				id: 'up_t2',
-				name: 'Trunk Name 2',
-				icon: 'icon-leaves',
-				description: 'Trunk Desc 2',
+				name: 'Compartmentalization of Decay',
+				icon: 'icon-wall',
+				description: 'Tissue cells form walls around wounds, preventing any damage from spreading.',
 				costs: [50, 100, 200],
 				bought: false,
 			}
 		]
 	},
 	'roots': {
-		description: 'This is important too...',
+		description: 'The roots anchor the tree to the ground, and take up nutrients and ' +
+								 '<span class="resource water">water</span> from the soil.',
 		items: [
 			{
 				id: 'up_r0',
-				name: 'Root Name 0',
-				icon: 'icon-branch',
-				description: 'Root Desc 0',
+				name: 'Mutualistic Aerobes',
+				icon: 'icon-bacteria',
+				description: 'Aerobic organisms populate the root microbiome, supplying nutrients and improving soil quality.',
 				costs: [50, 100, 200],
 				bought: false,
 			},
 			{
 				id: 'up_r1',
-				name: 'Root Name 1',
-				icon: 'icon-light',
-				description: 'Root Desc 1',
+				name: 'Mychorrizal Partnership',
+				icon: 'icon-root',
+				description: 'Symbiotic fungi intertwine the roots, improving absorption of nutrients in exchange for ' +
+				             '<span class="resource carb">carbohydrates.</span>',
 				costs: [50, 100, 200],
 				bought: false,
 			},
 			{
 				id: 'up_r2',
 				name: 'Root Name 2',
-				icon: 'icon-leaves',
+				icon: 'icon-bug',
 				description: 'Root Desc 2',
 				costs: [50, 100, 200],
 				bought: false,
